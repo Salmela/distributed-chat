de134d2ef7072e167039f00c383cea25aef77978
Aleksi Salmela <aleksi.salmela@giosg.com>
2024-11-22 18:06:15 +0200

    Write tests

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 #!/bin/python3
 
-import socket
+from socket import socket as os_socket
 import sys
 import json
 from threading import Thread
 
-if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} PEER_HOSTNAME")
-    exit(-1)
-
 APPLICATION_PORT = 65412
-PEER_HOST = sys.argv[1] # svm-11-3.cs.helsinki.fi
 
-def ui(peer_host, peer_port):
+def ui(peer_host, peer_port, input=input, socket=os_socket):
     while True:
         message = input("viestisi: ")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((peer_host, peer_port))
             s.sendall(json.dumps({"message": message}).encode())
             data = s.recv(1024)
 
             print(f"Received {data!r}")
 
-def start_server(port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def start_server(socket=os_socket):
+    with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("0.0.0.0", APPLICATION_PORT))
         s.listen()
         while True:
@@ -38,9 +33,16 @@ def start_server(port):
 
 # Only run this code if the file was executed from command line
 if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} PEER_HOSTNAME")
+        exit(-1)
+
+    peer_host = sys.argv[1] # svm-11-3.cs.helsinki.fi
+
     # We are creating separate threads for server and client so that they can run at same time. The sockets api is blocking.
-    t = Thread(target=ui, args=[PEER_HOST, APPLICATION_PORT])
+    t = Thread(target=ui, args=[peer_host, APPLICATION_PORT])
     t.start()
 
-    t = Thread(target=start_server, args=[APPLICATION_PORT])
+    t = Thread(target=start_server, args=[])
     t.start()
--- tests.py
+++ tests.py
@@ -1,9 +1,43 @@
 import unittest
+from unittest.mock import Mock, MagicMock
+from main import ui, start_server
 
-class TestStringMethods(unittest.TestCase):
+class UserInterfaceTestCase(unittest.TestCase):
 
-    def test_works(self):
-        self.assertEqual('foo', 'foo')
+    def test_send_message(self):
+        mock_input = Mock()
+        mock_input.side_effect = ["Cool example message", KeyboardInterrupt()]
+        socket_instance = Mock()
+        socket_instance.recv.return_value = 'Cool message from peer'
+
+        mock_socket_factory = Mock()
+        mock_socket_factory.return_value = MagicMock()
+        mock_socket_factory.return_value.__enter__.return_value = socket_instance
+
+        with self.assertRaises(KeyboardInterrupt):
+            ui('123.123.123.123', 456, input=mock_input, socket=mock_socket_factory)
+
+        socket_instance.connect.assert_called_with(('123.123.123.123', 456))
+        socket_instance.sendall.assert_called_with(b'{"message": "Cool example message"}')
+
+class ServerTestCase(unittest.TestCase):
+    def test_receive_connection(self):
+        peer_socket = MagicMock()
+        peer_socket.recv.side_effect = [b'Nice message from peer', '']
+
+        server_socket = Mock()
+        server_socket.accept.side_effect = [(peer_socket, None), KeyboardInterrupt]
+
+        mock_socket_factory = Mock()
+        mock_socket_factory.return_value = MagicMock()
+        mock_socket_factory.return_value.__enter__.return_value = server_socket
+
+        with self.assertRaises(KeyboardInterrupt):
+            start_server(socket=mock_socket_factory)
+
+        server_socket.bind.assert_called_with(('0.0.0.0', 65412))
+        server_socket.listen.assert_called_with()
+        peer_socket.sendall.assert_called_with(b'Nice message from peer')
 
 if __name__ == '__main__':
     unittest.main()
