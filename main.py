743571d1a1bab098c9898a8264a935a7d32cdfa8
Aleksi Salmela <aleksi.salmela@giosg.com>
2024-11-27 14:24:34 +0200

    Create Node class that contains nearly all the code

--- main.py
+++ main.py
@@ -8,50 +8,54 @@ import logging
 from threading import Thread
 
 APPLICATION_PORT = 65412
-message_queue = queue.Queue()
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename=f"chat.log", level=logging.DEBUG, format="%(asctime)s - %(message)s")
 
-def ui(peer_hosts, peer_port, nickname, input=input, socket=os_socket):
-    try:
-        while True:
-            message = input("viestisi: ")
-            for peer_host in peer_hosts:
-                with socket(AF_INET, SOCK_STREAM) as s:
-                    s.connect((peer_host, peer_port))
-                    s.sendall(json.dumps({"message": message, "sender":nickname}).encode())
-                    data = s.recv(1024)
+class Node:
+    def __init__(self, hosts):
+        self.message_queue = queue.Queue()
+        self.peer_hosts = hosts
 
-                    print()
-                    logger.debug(f"Sent by {nickname}: {data}")
-    except Exception as exc:
-        logger.exception(exc)
-        raise exc
-
-def start_server(socket=os_socket):
-    try:
-        with socket(AF_INET, SOCK_STREAM) as s:
-            s.bind(("0.0.0.0", APPLICATION_PORT))
-            s.listen()
+    def ui(self, peer_port, nickname, input=input, socket=os_socket):
+        try:
             while True:
-                conn, addr = s.accept()
-                with conn:
-                    while True:
-                        data = conn.recv(1024)
-                        if not data:
-                            break
-                        message = json.loads(data)
-                        print()
-                        print(f"Received by {data}")
-                        logger.debug(f"Received by {message['sender']}: {message}")
-                        message_queue.put(data)
+                message = input("viestisi: ")
+                for peer_host in self.peer_hosts:
+                    with socket(AF_INET, SOCK_STREAM) as s:
+                        s.connect((peer_host, peer_port))
+                        s.sendall(json.dumps({"message": message, "sender":nickname}).encode())
+                        data = s.recv(1024)
 
-                        if not message_queue.empty():
-                            message = message_queue.get()
-                            conn.sendall(message)
-    except Exception as exc:
-        logger.exception(exc)
-        raise exc
+                        print()
+                        logger.debug(f"Sent by {nickname}: {data}")
+        except Exception as exc:
+            logger.exception(exc)
+            raise exc
+
+    def start_server(self, socket=os_socket):
+        try:
+            with socket(AF_INET, SOCK_STREAM) as s:
+                s.bind(("0.0.0.0", APPLICATION_PORT))
+                s.listen()
+                while True:
+                    conn, addr = s.accept()
+                    with conn:
+                        while True:
+                            data = conn.recv(1024)
+                            if not data:
+                                break
+                            message = json.loads(data)
+                            print()
+                            print(f"Received by {data}")
+                            logger.debug(f"Received by {message['sender']}: {message}")
+                            self.message_queue.put(data)
+
+                            if not self.message_queue.empty():
+                                message = self.message_queue.get()
+                                conn.sendall(message)
+        except Exception as exc:
+            logger.exception(exc)
+            raise exc
 
 # Only run this code if the file was executed from command line
 if __name__ == '__main__':
@@ -61,12 +65,13 @@ if __name__ == '__main__':
         exit(-1)
 
     peer_hosts = sys.argv[1:] # svm-11-3.cs.helsinki.fi
+    node = Node(peer_hosts)
 
     nickname = input("Set nickname: ")
 
     # We are creating separate threads for server and client so that they can run at same time. The sockets api is blocking.
-    t = Thread(target=ui, args=[peer_hosts, APPLICATION_PORT, nickname])
+    t = Thread(target=node.ui, args=[APPLICATION_PORT, nickname])
     t.start()
 
-    t = Thread(target=start_server, args=[])
+    t = Thread(target=node.start_server, args=[])
     t.start()
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest.mock import Mock, MagicMock
-from main import ui, start_server
+from main import Node
 
 class UserInterfaceTestCase(unittest.TestCase):
 
     def test_send_message(self):
+        node = Node(['123.123.123.123'])
         mock_input = Mock()
         mock_input.side_effect = ["Cool example message", KeyboardInterrupt()]
         socket_instance = Mock()
@@ -15,13 +16,14 @@ class UserInterfaceTestCase(unittest.TestCase):
         mock_socket_factory.return_value.__enter__.return_value = socket_instance
 
         with self.assertRaises(KeyboardInterrupt):
-            ui(['123.123.123.123'], 456, nickname="Nick", input=mock_input, socket=mock_socket_factory)
+            node.ui(456, nickname="Nick", input=mock_input, socket=mock_socket_factory)
 
         socket_instance.connect.assert_called_with(('123.123.123.123', 456))
         socket_instance.sendall.assert_called_with(b'{"message": "Cool example message", "sender": "Nick"}')
 
 class ServerTestCase(unittest.TestCase):
     def test_receive_connection(self):
+        node = Node([])
         peer_socket = MagicMock()
         peer_socket.recv.side_effect = [b'{"message": "Nice message from peer", "sender": "Rick"}', ""]
 
@@ -33,7 +35,7 @@ class ServerTestCase(unittest.TestCase):
         mock_socket_factory.return_value.__enter__.return_value = server_socket
 
         with self.assertRaises(KeyboardInterrupt):
-            start_server(socket=mock_socket_factory)
+            node.start_server(socket=mock_socket_factory)
 
         server_socket.bind.assert_called_with(('0.0.0.0', 65412))
         server_socket.listen.assert_called_with()
